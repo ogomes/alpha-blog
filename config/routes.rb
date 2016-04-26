63ffad57adec9be28cf709fcca9e800114861ea9
Udemy Rails Course Otavio Colella <otavio.colella@gmail.com>
2016-04-26 22:25:34 +0000

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ get 'about', to:'pages#about'
 
 resources :articles
 
+get 'signup', to: 'users#new'
+resources :users, except: [:new]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
