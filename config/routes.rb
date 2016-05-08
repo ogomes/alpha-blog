3254e6deefd770f9a12371d6a397ef8d9c1637d2
Udemy Rails Course Otavio Colella <otavio.colella@gmail.com>
2016-05-08 16:19:18 +0000

    Add categories model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,16 @@ get 'about', to:'pages#about'
 resources :articles
 
 get 'signup', to: 'users#new'
+
 resources :users, except: [:new]
 
 get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
+
+
+resources :categories, except: [:destroy]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
